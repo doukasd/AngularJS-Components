c2cf53221b355c245ee0dbc8e72966efb9b9f698
leibler <github@sprossenwanne.at>
2015-10-27 20:06:16 +0100

    turn into module 'ddTextCollapse'; add newline to <br> feature;

--- dd-text-collapse/dd-text-collapse.js
+++ dd-text-collapse/dd-text-collapse.js
@@ -1,7 +1,9 @@
 // a directive to auto-collapse long text
 // in elements with the "dd-text-collapse" attribute
+// example: <div dd-text-collapse dd-text-collapse-max-length="250" dd-text-collapse-text="{{it.description}}"  dd-text-collapse-escape="true" dd-text-collapse-nl2br="true"></div>
 // default: escape text - turn off with dd-text-collapse-escape="false"
-app.directive('ddTextCollapse', ['$compile', function($compile) {
+// default: newlines to <br> - turn off with dd-text-collapse-nl2br="false"
+angular.module('ddTextCollapse', []).directive('ddTextCollapse', ['$compile', function($compile) {
 
     return {
         restrict: 'A',
@@ -32,14 +34,23 @@ app.directive('ddTextCollapse', ['$compile', function($compile) {
                 });
             }
 
-            // wait for changes on the text
-            attrs.$observe('ddTextCollapseText', function(text) {
-                // get the length from the attributes
+            scope.ddTextCollapseText = function(text) {
+               // get the length from the attributes
                 var maxLength = scope.$eval(attrs.ddTextCollapseMaxLength);
                 // check dd-text-collapse-escape attribute and use true as default
                 var escape = scope.$eval(attrs.ddTextCollapseEscape);
                 if( typeof(escape) == "undefined" ) {
                     escape = true;
+                }
+                // check dd-text-collapse-nl2br attribute and use true as default
+                var nl2br = scope.$eval(attrs.ddTextCollapseNl2br);
+                if( typeof(nl2br) == "undefined" ) {
+                    nl2br = true;
+                }
+                if( typeof(text) == "undefined" ) {
+                    text = ''+scope.text;
+                } else {
+                    scope.text = text;
                 }
                 if( escape ) {
                     text = scope.escapeHtml( text );
@@ -48,9 +59,17 @@ app.directive('ddTextCollapse', ['$compile', function($compile) {
                     // split the text in two parts, the first always showing
                     var firstPart = String(text).substring(0, maxLength);
                     var secondPart = String(text).substring(maxLength, text.length);
-
+                    if( nl2br ) {
+                        firstPartNl2Br = firstPart.replace(/\n/g, "<br />");
+                        secondPart = secondPart.replace(/\n/g, "<br />");
+                    }
                     // create some new html elements to hold the separate info
-                    var firstSpan = $compile('<span>' + firstPart + '</span>')(scope);
+                    if( nl2br ) {
+                        var firstSpan = $compile('<span  ng-if="!collapsed">' + firstPart + '</span>')(scope);
+                        var firstSpan2 = $compile('<span  ng-if="collapsed">' + firstPartNl2Br + '</span>')(scope);
+                    } else {
+                        var firstSpan = $compile('<span>' + firstPart + '</span>')(scope);
+                    }
                     var secondSpan = $compile('<span ng-if="collapsed">' + secondPart + '</span>')(scope);
                     var moreIndicatorSpan = $compile('<span ng-if="!collapsed">... </span>')(scope);
                     var lineBreak = $compile('<br ng-if="collapsed">')(scope);
@@ -60,6 +79,9 @@ app.directive('ddTextCollapse', ['$compile', function($compile) {
                     // and add the new ones we created
                     element.empty();
                     element.append(firstSpan);
+                    if( nl2br ) {
+                        element.append(firstSpan2);
+                    }
                     element.append(secondSpan);
                     element.append(moreIndicatorSpan);
                     element.append(lineBreak);
@@ -69,6 +91,11 @@ app.directive('ddTextCollapse', ['$compile', function($compile) {
                     element.empty();
                     element.append(text);
                 }
+            }
+
+            // wait for changes on the text
+            attrs.$observe('ddTextCollapseText', function(text) {
+                scope.ddTextCollapseText(text);
             });
         }
     };
